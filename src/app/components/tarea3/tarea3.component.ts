07da0ee7c840cd8f211b8702a9d0332f31fb4fff
Julio <julio.szl591@gmail.com>
2025-05-12 10:56:19 -0600

    Inicio de la base de los componentes

---
+++ src/app/components/tarea3/tarea3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea3',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarea3.component.html',
+  styleUrl: './tarea3.component.css'
+})
+export class Tarea3Component {
+
+}
