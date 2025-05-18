45112f0c251d06a0dba58779dfac2a34dec23ad1
Julio-szl <julio.szl591@gmail.com>
2025-05-18 04:05:11 -0600

    Inicio de la pagina home y configuracion de bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NavComponent } from './shared/nav/nav.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: NavComponent}
+];
--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 
+  scrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset || document.documentElement.scrollTop;
+    this.scrolled = offset > 400;
+  }
+
+  ngOnInit() {
+    this.onWindowScroll();
+  }
 }
